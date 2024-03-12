8a3fa041a8a00c0dbbcd831362341b0fdda5dad0
Michał Sekuła <michlo23@gmail.com>
2024-03-12 08:26:35 +0100

    Added express-validator and routing to Auth

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,13 +1,23 @@
-import express from "express";
-import { json } from "body-parser";
+import express from 'express';
+import { json } from 'body-parser';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
 
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send({ user: "Endrju" });
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// app.get('/api/users/currentuser', (req, res) => {
+//   res.send({ user: 'Endrju' });
+// });
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log('Listening on port 3000');
 });
---
+++ ticketing/auth/src/routes/signup.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+
+const router = express.Router();
+
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    // new User({email, password})
+    res.send({ user: 'Endrju1' });
+  }
+);
+
+export { router as signupRouter };
