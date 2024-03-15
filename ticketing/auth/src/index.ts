0405fb1015c6d7413cf1f72433c8bab8460bd3b2
Michał Sekuła <michlo23@gmail.com>
2024-03-15 06:19:00 +0100

    Added auth-mongo service

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import 'express-async-errors';
+import 'express-async-errors'; // fixing async error handling in express
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -21,7 +22,16 @@ app.all('*', async () => {
 });
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log('Listening on port 3000');
+  });
+}
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+start();
