eb50507720f156f7a0fb43cf0ec286c9b4a789e7
Michał Sekuła <michlo23@gmail.com>
2024-03-22 13:00:13 +0100

    Created ValidateRequest middleware

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,9 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send({ user: 'Endrju1' });
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('Password need to be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Incorrect password or email');
+    }
+    res.send({ user: 'Endrju1' });
+  }
+);
 
 export { router as signinRouter };
