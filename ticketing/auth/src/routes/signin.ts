f4e879fc1afcafe71a9fc06937ba1dd33ad6338f
Michał Sekuła <michlo23@gmail.com>
2024-03-23 20:39:01 +0100

    Implemented SignIn function

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -3,6 +3,8 @@ import { body } from 'express-validator';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middlewares/validate-request';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -24,7 +26,31 @@ router.post(
     if (!existingUser) {
       throw new BadRequestError('Incorrect password or email');
     }
-    res.send({ user: 'Endrju1' });
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError('Incorrect password or email');
+    }
+
+    //Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    //Store it in on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
   }
 );
 
