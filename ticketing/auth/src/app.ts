7049cbf790d13336133073862841adbf9bdc3b51
Michał Sekuła <michlo23@gmail.com>
2024-03-26 12:52:03 +0100

    Separate express app logic from launching configuration

---
+++ ticketing/auth/src/app.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import 'express-async-errors'; // fixing async error handling in express
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+//ensure that express will trust ingress as proxy
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
+
+/*
+FROM node:alpine
+ 
+WORKDIR /app
+COPY package.json .
+RUN npm install --omit=dev
+COPY . .
+ 
+CMD ["npm", "start"]
+*/
--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,36 +1,5 @@
-import express from 'express';
-import 'express-async-errors'; // fixing async error handling in express
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-//ensure that express will trust ingress as proxy
-app.set('trust proxy', true);
-
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
