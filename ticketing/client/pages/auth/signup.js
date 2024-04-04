dc45eb06919274722cce0a10adea114f2a8d7f19
Michał Sekuła <michlo23@gmail.com>
2024-04-04 11:48:52 +0200

    Created useRequest hook

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,26 +1,21 @@
 import { useState } from 'react';
-import axios from 'axios';
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
 
 const signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: { email, password },
+    onSuccess: () => Router.push('/'),
+  });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password,
-      });
-
-      console.log(response.data);
-    } catch (err) {
-      console.log(err.response.data);
-      setErrors(err.response.data.errors);
-    }
-    console.log(errors);
+    doRequest();
   };
 
   return (
@@ -43,16 +38,7 @@ const signup = () => {
           className='form-control'
         ></input>
       </div>
-      {errors.length > 0 && (
-        <div className='alert alert-danger'>
-          <h4>Ooops....</h4>
-          <ul className='my-0'>
-            {errors.map((err) => {
-              return <li key={err.message}>{err.message}</li>;
-            })}
-          </ul>
-        </div>
-      )}
+      {errors}
 
       <button className='btn btn-primary'>Sign up</button>
     </form>
