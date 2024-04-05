6a2c1a307e72b579b4d3d0d18391b2481b3439f4
Michał Sekuła <michlo23@gmail.com>
2024-04-05 14:32:38 +0200

    Handling current user in landing page

---
+++ ticketing/client/api/build-client.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const buildClient = ({ req }) => {
+  if (typeof window === 'undefined') {
+    //we are on the server
+    return axios.create({
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers,
+    });
+  } else {
+    //we are on the brwoser
+    return axios.create({
+      baseURL: '/',
+    });
+  }
+};
+
+export default buildClient;
--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,5 +1,18 @@
-const landingPage = () =>{
-    return <h1>Landing Page!!!</h1>
+import buildClient from '../api/build-client';
+
+const landingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing Page!!!</h1>;
+};
+
+// method called before page will be rendered (server side rendering) UNLESS it was redirected from subpage...
+landingPage.getInitialProps = async ({ req }) => {
+  var axios = buildClient({ req });
+  const { data } = await axios.get('/api/users/currentuser').catch((err) => {
+    console.log(err.message);
+  });
+
+  return data;
 };
 
 export default landingPage;
